c64407b5228bf81d79e016ba1df20c11c366ee28
Sanovski44 <sanovski44@gmail.com>
2021-11-01 14:52:10 +0300

    Initial commit

---
+++ features/environment.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import os
+
+
+def before_all(context):
+    chrome_option = webdriver.ChromeOptions()
+    if os.environ.get('docker') == 'true':
+        print("Init docker env")
+        chrome_option.add_argument("headless")
+        chrome_option.add_argument("--no-sandbox")
+        chrome_option.add_argument("--disable-dev-shm-usage")
+        context.driver = webdriver.Chrome(chrome_options=chrome_option)
+    else:
+        chrome_option.add_argument("--start-maximized")
+        context.driver = webdriver.Chrome(chrome_options=chrome_option)
+
+    context.driver.implicitly_wait(5)
+
+
+def after_all(context):
+    context.driver.quit()
+
---
+++ features/steps/AutoTest.py
@@ -0,0 +1,131 @@
+from behave import *
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import random
+
+
+def rand():
+    rnd = random.randint(1, 10000)
+    return rnd
+
+
+url = "http://qa-assignment.oblakogroup.ru/board/:alexandr_vizgunov" + str(rand())
+
+
+@given("Открыть страницу")
+def step_impl(context):
+    context.driver.get(url)
+
+
+@then("Должна открыться страница")
+def step_impl(context):
+    heading = context.driver.find_element_by_class_name("title")
+    assert heading.is_displayed()
+
+
+@when("Нажать на кнопку +")
+def step_impl(context):
+    button = context.driver.find_element_by_id("add_new_todo")
+    button.click()
+
+
+@step("Ввести название задачи '{text}'")
+def step_impl(context, text):
+    task = context.driver.find_element_by_id("project_text")
+    task.clear()
+    task.send_keys(text)
+
+
+@step("Выбрать категорию '{text}'")
+def step_impl(context, text):
+    category = context.driver.find_element_by_id("select2-select_category-container")
+    category.click()
+    category_name = context.driver.find_element_by_xpath('//*[contains(text(), "' + text + '")]')
+    category_name.click()
+
+
+@step("Нажать на кнопку OK")
+def step_impl(context):
+    ok_button = context.driver.find_element_by_id("submit_add_todo")
+    context.driver.execute_script("arguments[0].click();", ok_button)
+
+
+@then("На странице есть задача '{text}'")
+def step_impl(context, text):
+    tasks = context.driver.find_elements_by_id("todo_text")
+    elements = list(filter(lambda x: x.find_element_by_tag_name("label").text == text, tasks))
+    assert len(elements) == 1
+    assert elements[0].is_displayed()
+
+
+@step('Ввести название задачи (например "!@#$%^")')
+def step_impl(context):
+    task = context.driver.find_element_by_id("project_text")
+    task.clear()
+    task.send_keys("!@#$%^")
+
+
+@step("Задача не выполнена '{text}'")
+def step_impl(context, text):
+    task = context.driver.find_element_by_xpath(
+        '//*[ @ class = "icheckbox_square-blue"]/ancestor::div//*[contains(text(), "' + text + '")]')
+    id = task.get_attribute("id")
+    task = context.driver.find_element_by_id(id)
+    assert task.is_selected() is False
+
+
+@when("Изменить статус задачи '{text}'")
+def step_impl(context, text):
+    task = context.driver.find_element_by_xpath(
+        '//*[ @ id = "todo_check"]/ancestor::div//*[contains(text(), "' + text + '")]')
+    task.click()
+
+
+@then("Задача выполнена '{text}'")
+def step_impl(context, text):
+    task = context.driver.find_element_by_xpath(
+        '//*[ @ class = "icheckbox_square-blue checked"]/ancestor::div//*[contains(text(), "' + text + '")]')
+    id = task.get_attribute("id")
+    task = context.driver.find_element_by_id(id)
+    assert task.is_selected()
+
+
+@step("Задача выполнена '{text}'")
+def step_impl(context, text):
+    task = context.driver.find_element_by_xpath(
+        '//*[ @ class = "icheckbox_square-blue checked"]/ancestor::div//*[contains(text(), "' + text + '")]')
+    id = task.get_attribute("id")
+    task = context.driver.find_element_by_id(id)
+    assert task.is_selected()
+
+
+@step("Ввести заголоваок категории '{text}'")
+def step_impl(context, text):
+    new_category = context.driver.find_element_by_id("project_title")
+    assert new_category.is_displayed()
+    new_category.clear()
+    new_category.send_keys(text)
+
+
+@then("На странице есть  категория '{text}'")
+def step_impl(context, text):
+    categories = context.driver.find_elements_by_class_name("shadow_todos")
+    elements = list(filter(lambda x: x.find_element_by_tag_name("h2").text == text, categories))
+    assert len(elements) == 1
+    assert elements[0].is_displayed()
+
+
+@given('we have behave installed')
+def step_impl(context):
+    pass
+
+
+@when('we implement a test')
+def step_impl(context):
+    assert True is not False
+
+
+@then('behave will test it for us!')
+def step_impl(context):
+    assert context.failed is True
